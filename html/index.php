67825b51de70b3945ee0112ae08390be89a92fa5
brthiess <brthiess@ualberta.ca>
2015-02-13 16:01:45 -0700

    Created Very Basic Homepage

---
+++ html/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>The Radiology Clinic</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <a class="navbar-brand" href="#">
+			The Radiology Clinic
+		  </a>
+		</div>
+	  </div>
+	</nav>
+
+	<div class="container-fluid">
+		<div class="row">
+			<h1 class="text-center">Welcome to the Radiology Clinic</h1>
+		</div>
+		<div class="row">
+			<div class="col-md-2 col-md-offset-5"> 
+				<button type="button" class="btn btn-block btn-info"><h4>Login</h4></button> 
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
